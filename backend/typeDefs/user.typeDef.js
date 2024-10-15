17ac2053e287532eded3d12979b3eae4177f953f
Pratik Kamble <prak@ciklum.com>
2024-10-15 23:38:32 +0530

    Crated User schema typeDefs

---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,39 @@
+// after key name ! mark symbol indicates this filed is required ( not nullable )
+
+const userTypeDef = `#graphql
+        type User {
+            _id: ID!,
+            username: String!,
+            password: String!,
+            profilePicture:String,
+            gender: String!,
+        }
+
+        type query {
+            users:[User!],
+            authenticatedUser:User,
+            user(userId:Id!): User
+        }
+
+        type Mutation {
+            signup(input: SignupInput!): User
+            login(input: LoginInput!): User
+            logout: LogoutResponse
+        }
+
+        input SignupInput {
+            username: String!
+            name: String!
+            password: String!
+            gender: String!
+        }
+
+        input LoginInput {
+            username: String!
+            password: String!
+        }
+
+        type LogoutResponse {
+            message: String!
+        }
+    `;
